619be768deb445bbd703c7555327220ea7e6c1d9
jguadagno <jguadagno@hotmail.com>
2022-11-11 02:45:10 -0700

    Added the missing DebuggerBrowsable

--- src/MasteringDebugging/Person.cs
+++ src/MasteringDebugging/Person.cs
@@ -9,6 +9,9 @@ public class Person
     public string MiddleInitial { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
+    #region Hidden for now
+    //[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    #endregion
     public DateTime DateOfBirth { get; set; }
     public List<Person> Children { get; set; }
 
