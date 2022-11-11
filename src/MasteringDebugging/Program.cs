3aae97aae9e596440e4fb22170804d98e52c03c5
jguadagno <jguadagno@hotmail.com>
2022-11-11 02:45:44 -0700

    Removed the try/catch

--- src/MasteringDebugging/Program.cs
+++ src/MasteringDebugging/Program.cs
@@ -1,28 +1,26 @@
 ﻿using MasteringDebugging;
 
 Console.WriteLine("Hello, World!");
-try
-{
-    List<Person> people = PopulatePeople();
 
-            foreach (var person in people)
-            {
-                Console.WriteLine(person.FullName);
-            } 
-            var parentCount = people.Where(p => p.LastName =="Guadagno").Count(p => p.Children is not null);
-            Console.WriteLine($"Number of parents is {parentCount}");
-            Console.WriteLine("Press Enter");
-            Console.ReadLine();
-            Console.WriteLine("Done");
-        }
-        static List<Person> PopulatePeople ()
-        {
-            return new List<Person> {
-                new Person {FirstName = "Joseph", MiddleInitial="J", LastName="Guadagno", Email="jguadagno@hotmail.com", DateOfBirth= DateTime.Parse("05/31/1971 2:17 am"), Children = new List<Person> {new Person {FirstName = "JJ"}}},
-                new Person {FirstName = "Deidre", MiddleInitial="A", LastName="Guadagno", Email="deeguadagno@hotmail.com", DateOfBirth= DateTime.Parse("05/03/1970")},
-                new Person {FirstName = "Joseph", MiddleInitial="J", LastName="Guadagno", Email="jjguadagno@gmail.com", DateOfBirth= DateTime.Parse("05/31/1997")},
-                new Person {FirstName = "Emily", MiddleInitial="E", LastName="Guadagno", Email="eeguadagno@msn.com", DateOfBirth= DateTime.Parse("05/31/1999")}
-            };
-        }
-    }
+List<Person> people = PopulatePeople();
+
+foreach (var person in people)
+{
+    Console.WriteLine(person.FullName);
+}
+
+var parentCount = people.Where(p => p.LastName == "Guadagno").Count(p => p.Children is not null && p.Children.Any());
+Console.WriteLine($"Number of parents is {parentCount}");
+Console.WriteLine("Press Enter");
+Console.ReadLine();
+Console.WriteLine("Done");
+
+static List<Person> PopulatePeople ()
+{
+    return new List<Person> {
+        new Person {FirstName = "Joseph", MiddleInitial="J", LastName="Guadagno", Email="jguadagno@hotmail.com", DateOfBirth= DateTime.Parse("05/31/1971 2:17 am"), Children = new List<Person> {new Person {FirstName = "JJ"}}},
+        new Person {FirstName = "Deidre", MiddleInitial="A", LastName="Guadagno", Email="deeguadagno@hotmail.com", DateOfBirth= DateTime.Parse("05/03/1970")},
+        new Person {FirstName = "Joseph", MiddleInitial="J", LastName="Guadagno", Email="jjguadagno@gmail.com", DateOfBirth= DateTime.Parse("05/31/1997")},
+        new Person {FirstName = "Emily", MiddleInitial="E", LastName="Guadagno", Email="eeguadagno@msn.com", DateOfBirth= DateTime.Parse("05/31/1999")}
+    };
 }
